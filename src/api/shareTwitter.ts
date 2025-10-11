76bc7c016435199f1efa894e4786a24b9191d3c0
kalitva <33896868+pavave@users.noreply.github.com>
2025-10-11 07:04:39 +0300

    Update shareTwitter.ts

--- src/api/shareTwitter.ts
+++ src/api/shareTwitter.ts
@@ -1,1 +1,19 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const { forecastText } = req.body;
+  const bearerToken = process.env.TWITTER_BEARER;
+
+  const response = await fetch("https://api.twitter.com/2/tweets", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: forecastText }),
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
 
