dac87ea55f47c91b8a5719383e0d0ef04759c6fc
kalitva <33896868+pavave@users.noreply.github.com>
2025-10-11 07:05:08 +0300

    Update fetchBinance.ts

--- src/data/fetchBinance.ts
+++ src/data/fetchBinance.ts
@@ -1,1 +1,10 @@
+import ccxt from "ccxt";
+
+export async function fetchBinanceOHLCV(pair: string, timeframe = "1d", since?: number) {
+  const binance = new ccxt.binance();
+  const ohlcv = await binance.fetchOHLCV(pair, timeframe, since);
+  return ohlcv.map(([timestamp, open, high, low, close, volume]) => ({
+    timestamp, open, high, low, close, volume,
+  }));
+}
 
