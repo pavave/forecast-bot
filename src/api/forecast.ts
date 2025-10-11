96149ece35a270b5cfa74e24e72e9013dd9ee079
kalitva <33896868+pavave@users.noreply.github.com>
2025-10-11 07:03:44 +0300

    Update forecast.ts

--- src/api/forecast.ts
+++ src/api/forecast.ts
@@ -1,1 +1,8 @@
+import { ForecastEngine } from "../ForecastEngine";
+
+export default async function handler(req, res) {
+  const { pair } = req.query;
+  const forecast = await new ForecastEngine().run(pair);
+  res.status(200).json({ forecast });
+}
 
